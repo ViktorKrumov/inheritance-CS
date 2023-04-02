7c7923bdb9a630e3b93992a7e0935b2bc3dd0231
ViktorKrumov <80381396+ViktorKrumov@users.noreply.github.com>
2023-04-02 18:03:56 +0300

    Add files via upload

---
+++ Person/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace InheritancePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string childName = Console.ReadLine();
+            int childAge = int.Parse(Console.ReadLine());
+
+            string motherName = Console.ReadLine();
+            int motherAGe = int.Parse(Console.ReadLine());
+
+            string fatherName = Console.ReadLine();
+            int fatherAge = int.Parse(Console.ReadLine());
+
+            var mother = new Person(motherName, motherAGe);
+            var father = new Person(fatherName, fatherAge);
+            var child = new Child(childName, childAge, mother, father);
+            Console.WriteLine(child.ToString());
+        }
+    }
+}
