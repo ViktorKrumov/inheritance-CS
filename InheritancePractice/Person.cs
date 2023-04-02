4787a664ff96e83cd92803430005934ec34be35e
ViktorKrumov <80381396+ViktorKrumov@users.noreply.github.com>
2023-04-02 18:00:19 +0300

    Add files via upload

---
+++ InheritancePractice/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InheritancePractice
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder stringBul = new StringBuilder();
+            stringBul.Append(String.Format("Name: {0}, Age: {1}", this.Name, this.Age));
+            return stringBul.ToString();
+        }
+    }
+}
